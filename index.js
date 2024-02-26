9c44196cab27e9d865cbf1dba912e985ae819384
Rabrolino27 <107254791+Rabrolino27@users.noreply.github.com>
2024-02-26 07:29:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,43 +10,7 @@ puppeteer.use(StealthPlugin())
 const {executablePath} = require('puppeteer')
 const app = express();
 
-app.get('/test', (req,res) => {
-    res.send('i am runnig fine');
-});
 
-
-app.get('/test3', (req, res) => {
-    (async () => {
-        // try{
-            const browser = puppeteer.launch({ executablePath: executablePath(),
-             } ).then(async browser => {
-                console.log('Running tests..')
-                const page = await browser.newPage()
-                 var response = await page.goto(req.query.url);
-                if(req.query.type == 'HTML' || req.query.type == 'XML'){
-                    res.json({
-                        data: await page.content(),
-                        headers : response.headers(),
-                        cookies : page.cookies()
-                })
-                }else if(req.query.type == 'JSON'){
-                  res.json({
-                        data: JSON.parse(await  page.evaluate(() => document.querySelector("body").innerText)),
-                        headers : response.headers(),
-                        cookies : page.cookies()
-                  })
-                }
-                await browser.close()
-                console.log(`All done,  ✨`)
-            
-          })
-        // }catch(e){
-        //     console.log(e)
-        // }
-  
-})();
-   
-});
 app.post('/', (req, res) => {
     (async () => {
         try{
